9eaeae7f6e1175b59f27f87410a38d7111483068
Enikansayemayowa <enikansayemayowa405@gmail.com>
2023-09-28 02:42:00 -0700

    fix betty error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,32 @@
 #include "sort.h"
 #include <stdbool.h>
-void swap(int* b, int* c)
+/**
+ * swap - swap integer in an array
+ * @b: first interger
+ * @c: second integer
+ * Return 0 or 1
+ */
+void swap(int *b, int *c)
 {
 	int temp = *b;
 	*b = *c;
 	*c = temp;
 }
-
+/**
+ * bubble_sort - compare each pair
+ * @size: this is the size of the array
+ * @array: array to be sorted
+ * Return: 0
+ */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j; 
+	size_t i, j;
 	int swapValue;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		swapValue = false;
-		for (j = i; j < size - i -1; j++)
+		for (j = i; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + i])
 			{
